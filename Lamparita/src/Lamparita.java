b70d1e78bdd6a2165e3c9de4f1516a02b2a15917
Facu Jara <gajara@estudiantes.unsam.edu.ar>
2023-09-11 22:18:10 -0300

    Espero que funcione

--- Lamparita/src/Lamparita.java
+++ Lamparita/src/Lamparita.java
@@ -1,4 +1,18 @@
 public class Lamparita {
+    private boolean estado;
+
+    Lamparita(){
+        this.estado = false;
+    }
+    public void encender(){
+        this.estado = true;
+    }
+    public void apagar(){
+        this.estado = false;
+    }
+    public boolean estado(){
+        return this.estado;
+    }
     public static void main(String[] args) throws Exception {
         System.out.println("Vamos a programar una lamparita! ");
     }
